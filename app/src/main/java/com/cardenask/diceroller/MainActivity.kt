fbd370ed767ffe08b4b5a896c37221b295c9e1cb
Kevin Cardenas <kcard13@terpmail.umd.edu>
2019-06-28 11:24:29 -0400

    Finished the DiceRoller tutorial. Fully functional now.

--- app/src/main/java/com/cardenask/diceroller/MainActivity.kt
+++ app/src/main/java/com/cardenask/diceroller/MainActivity.kt
@@ -3,32 +3,41 @@ package com.cardenask.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var diceImgView : ImageView // Limit the calls to the findViewById. Use lateinit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = main_roll_button
-        val tvValue : TextView = main_number_tv
-        
+        diceImgView = dice_imgview // Actually instantiating the ImageView once. Not every single time in roll dice
+
         rollButton.setOnClickListener {
             //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
-            rollDice(tvValue)
+            rollDice()
         }
-
     }
 
-    private fun rollDice(tvValue : TextView) {
+    private fun rollDice() {
 
-        val diceVal : Int = Random.nextInt(1, 6)
+        val diceVal : Int = Random.nextInt(1, 7) // [from, until)
+        val drawableImg : Int = when (diceVal) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
 
-        tvValue.text = diceVal.toString()
+        diceImgView.setImageResource(drawableImg)
     }
 }
